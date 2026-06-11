0765200f6ebe83c89766f70b4a0f2a8f4b93248b
Oliver Howard <oliver.t.howard@icloud.com>
2026-06-11 09:14:41 -0700

    fix: center favicon chevron

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./pixel.css";
 import App from "./App";
 
-const chevron = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><rect width="16" height="16" fill="#0d1117"/><rect x="3" y="1" width="2" height="2" fill="#4fa3e0"/><rect x="5" y="3" width="2" height="2" fill="#4fa3e0"/><rect x="7" y="5" width="2" height="2" fill="#4fa3e0"/><rect x="5" y="7" width="2" height="2" fill="#4fa3e0"/><rect x="3" y="9" width="2" height="2" fill="#4fa3e0"/></svg>`;
+const chevron = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><rect width="16" height="16" fill="#0d1117"/><rect x="5" y="3" width="2" height="2" fill="#4fa3e0"/><rect x="7" y="5" width="2" height="2" fill="#4fa3e0"/><rect x="9" y="7" width="2" height="2" fill="#4fa3e0"/><rect x="7" y="9" width="2" height="2" fill="#4fa3e0"/><rect x="5" y="11" width="2" height="2" fill="#4fa3e0"/></svg>`;
 const blank = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><rect width="16" height="16" fill="#0d1117"/></svg>`;
 const frames = [chevron, blank];
 const favicon = document.querySelector("link[rel='icon']") as HTMLLinkElement;
